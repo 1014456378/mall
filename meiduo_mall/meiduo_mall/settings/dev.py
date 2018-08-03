3c4597011f6a9d2f142f5b2b4d0ab7cd32caaa7c
lihao <1014456378@qq.com>
2018-08-03 19:55:45 +0800

    qq登陆功能

--- meiduo_mall/meiduo_mall/settings/dev.py
+++ meiduo_mall/meiduo_mall/settings/dev.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'corsheaders',
     'users.apps.UsersConfig',
     'verifications.apps.VerificationsConfig',
+    'oauth.apps.OauthConfig',
 ]
 
 MIDDLEWARE = [
@@ -241,4 +242,8 @@ CORS_ORIGIN_WHITELIST = (
 )
 #跨域请求允许携带cookie
 CORS_ALLOW_CREDENTIALS = True
-
+# QQ登录参数
+QQ_CLIENT_ID = '101474184'   #开发应用ID
+QQ_CLIENT_SECRET = 'c6ce949e04e12ecc909ae6a8b09b637c'
+QQ_REDIRECT_URI = 'http://www.meiduo.site:8080/oauth_callback.html'
+QQ_STATE = '/'
---
+++ meiduo_mall/meiduo_mall/utils/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+
+class BaseModel(models.Model):
+    creat_time = models.DateField(auto_now_add=True,verbose_name='创建时间')
+    updata_time = models.DateField(auto_now=True,verbose_name='更新时间')
+
+    class Meta:
+        abstract = True   #说明是抽象类，迁移时不会创建对应的表
