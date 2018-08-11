113484c2c73deadd65c2883d4ed03e4b22d46b8f
lihao <1014456378@qq.com>
2018-08-11 17:17:52 +0800

    购物车改查和全选

--- meiduo_mall/meiduo_mall/apps/carts/serializers.py
+++ meiduo_mall/meiduo_mall/apps/carts/serializers.py
@@ -5,7 +5,7 @@ from goods.models import SKU
 
 class CartSerializer(serializers.Serializer):
     sku_id = serializers.IntegerField(label='stu_id',min_value=1)
-    count = serializers.IntegerField(label='stu_id',min_value=1)
+    count = serializers.IntegerField(label='数量',min_value=1)
     selected = serializers.BooleanField(label='是否勾选',default=True)
 
     def validate(self, attrs):
@@ -23,3 +23,25 @@ class CartSKUSerializer(serializers.ModelSerializer):
     class Meta:
         model = SKU
         fields = ('id','count','selected','name','default_image_url','price')
+
+class CartDeleteSerializer(serializers.Serializer):
+    sku_id = serializers.IntegerField(label='sku_id',min_value=1)
+
+    def validate_sku_id(self, data):
+        sku_id = data
+        try:
+            sku = SKU.objects.get(id = sku_id)
+        except SKU.DoesNotExist:
+            raise serializers.ValidationError('商品不存在')
+        return data
+
+class CartSelectAllSerializer(serializers.Serializer):
+    """
+    购物车全选
+    """
+    selected = serializers.BooleanField(label='全选')
+
+
+
+
+
